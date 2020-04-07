6a23ce1ab39a408c1f1736fc9c645946bc66c861
Nikhil Gupta <mywork.ng@gmail.com>
2020-04-07 01:15:22 -0500

    made lattice work (was giving issues earlier)

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,6 +3,17 @@ library(testthat)
 library(tswge)
 library(tswgewrapped)
 
+## This is absolutely needed, else we get errors that "there is no package called 'lattice'" in the unit tests
+if(!require("lattice")){
+  # https://github.com/topepo/caret/issues/411#issuecomment-209973908
+  install.packages("lattice", repos = "http://cran.us.r-project.org", dependencies = c("Depends", "Imports", "Suggests"))
+}
+
+if(!require("forecast")){
+  # https://github.com/topepo/caret/issues/411#issuecomment-209973908
+  install.packages("forecast", repos = "http://cran.us.r-project.org", dependencies = c("Depends", "Imports", "Suggests"))
+}
+
 file = system.file("extdata", "USeconomic.csv", package = "tswgewrapped", mustWork = TRUE)
 USeconomic = read.csv(file, header = TRUE, stringsAsFactors = FALSE, check.names = FALSE)
 names(USeconomic) = gsub("[(|)]", "", colnames(USeconomic))
--- tests/testthat/test-BuildNNforCaret.R
+++ tests/testthat/test-BuildNNforCaret.R
@@ -7,33 +7,86 @@
 ## TODO: For now, we are predicting with the actual future values of xreg.
 ##       In the future, change this to use forecasted values of xreg
 ##       This can probably be achieved by passing xreg_name_predicted in the data and adjusting the source_caret_nnfor file accordingly
-   
+
+test_that("Random Serial", {
+  # http://r-pkgs.had.co.nz/tests.html
+  # TODO: Disable test before submitting to CRAN
+  # testthat::skip_on_cran()
+  
+  # Load Data
+  data = USeconomic
+
+  model = ModelBuildNNforCaret$new(data = data, var_interest = "logGNP", m = 4,
+                                   search = 'random',
+                                   grid = NA, tuneLength = 2,
+                                   batch_size = 136, h = 2,
+                                   parallel = TRUE,
+                                   seed = 1,
+                                   verbose = 1)
+
+  model$summarize_hyperparam_results()
+  model$summarize_best_hyperparams()
+  model$plot_hyperparam_results()
+  model$summarize_build()
+
+  good1 = TRUE
+  testthat::expect_equal(good1, TRUE)
+
+  good2 = TRUE
+  testthat::expect_equal(good2, TRUE)
+
+})
+
+# 
+# test_that("Grid Serial", {
+#   # http://r-pkgs.had.co.nz/tests.html
+#   # skip_on_cran()
+#   
+#   # Load Data
+#   data = USeconomic
+#   
+#   library(caret)
+#   
+#   nnfor_grid = expand.grid(reps = c(20),
+#                            hd = c(1:2),
+#                            allow.det.season = c(FALSE))
+#   
+#   model = ModelBuildNNforCaret$new(data = data, var_interest = "logGNP", m = 4,
+#                                    search = 'grid',
+#                                    grid = nnfor_grid,  # Use default grid
+#                                    batch_size = 136, h = 2,
+#                                    parallel = FALSE,
+#                                    seed = 1,
+#                                    verbose = 1)
+#   
+#   # # #testthat::expect_equal(good, TRUE)
+# 
+# })
+# 
+#
 # test_that("Random Parallel", {
 #   # http://r-pkgs.had.co.nz/tests.html
 #   # skip_on_cran()
-# 
+#   
 #   # Load Data
 #   data = USeconomic
-# 
+#   
 #   library(caret)
-# 
-#   # Random Parallel
-#   model = ModelBuildNNforCaret$new(data = data, var_interest = "logGNP", m = 2,
+#   
+#   model = ModelBuildNNforCaret$new(data = data, var_interest = "logGNP", m = 4,
 #                                    search = 'random',
 #                                    grid = NA, tuneLength = 2,
 #                                    batch_size = 136, h = 2,
 #                                    parallel = TRUE,
 #                                    seed = 1,
 #                                    verbose = 1)
-# 
-#   model$summarize_hyperparam_results()
-#   model$summarize_best_hyperparams()
-#   model$plot_hyperparam_results()
-#   model$summarize_build()
-# 
+#   
+#   
 #   # # #testthat::expect_equal(good, TRUE)
-# 
+#   
+#   
 # })
+#
 # 
 # 
 # test_that("Grid Parallel", {
@@ -45,8 +98,7 @@
 #   
 #   library(caret)
 #   
-#   # Grid Parallel
-#   model = ModelBuildNNforCaret$new(data = data, var_interest = "logGNP", m = 2,
+#   model = ModelBuildNNforCaret$new(data = data, var_interest = "logGNP", m = 4,
 #                                    search = 'grid',
 #                                    grid = NA, # Default Grid
 #                                    batch_size = 136, h = 2,
@@ -59,55 +111,4 @@
 #   
 # })
 # 
-# 
-# test_that("Random Serial", {
-#   # http://r-pkgs.had.co.nz/tests.html
-#   # skip_on_cran()
-#   
-#   # Load Data
-#   data = USeconomic
-#   
-#   library(caret)
-#   
-#   # Random Serial
-#   model = ModelBuildNNforCaret$new(data = data, var_interest = "logGNP", m = 2,
-#                                    search = 'random',
-#                                    grid = NA, tuneLength = 2,
-#                                    batch_size = 136, h = 2,
-#                                    parallel = FALSE,
-#                                    seed = 1,
-#                                    verbose = 1)
-#   
-#   
-#   # # #testthat::expect_equal(good, TRUE)
-#   
-#   
-# })
-# 
-# 
-# test_that("Grid Serial", {
-#   # http://r-pkgs.had.co.nz/tests.html
-#   # skip_on_cran()
-#   
-#   # Load Data
-#   data = USeconomic
-#   
-#   library(caret)
-#   
-#   # Grid Serial
-#   nnfor_grid = expand.grid(reps = c(20),
-#                            hd = c(1:2),
-#                            allow.det.season = c(FALSE))
-#   
-#   model = ModelBuildNNforCaret$new(data = data, var_interest = "logGNP", m = 2,
-#                                    search = 'grid',
-#                                    grid = nnfor_grid,  # Use default grid
-#                                    batch_size = 136, h = 2,
-#                                    parallel = FALSE,
-#                                    seed = 1,
-#                                    verbose = 1)
-#   
-#   # # #testthat::expect_equal(good, TRUE)
-# 
-# })
 # 
